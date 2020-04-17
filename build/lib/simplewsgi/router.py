f353032c909ab423b253e1f9ccf6a4d23106c446
eita-chinen <q304023zm@gmail.com>
2020-04-17 21:08:06 +0900

    Update build/lib/simplewsgi/router.py

--- build/lib/simplewsgi/router.py
+++ build/lib/simplewsgi/router.py
@@ -2,7 +2,8 @@ import re
 
 def http404(env, start_response):
     start_response('404 Not Found', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'404 Not Found']
+    return [b'404 not found']
+
 
 
 def http405(env, start_response):
